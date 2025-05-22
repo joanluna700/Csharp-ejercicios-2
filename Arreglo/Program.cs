ea009939df2fb5abf0f958629a64f9c6b956e212
Joan Santos <jsantosluna06@gmail.com>
2025-05-21 23:28:22 -0400

    Terminando el programa ordenamiento del arreglo.

---
+++ Arreglo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            Console.WriteLine("Ingrese 10 numeros desorenados: ");
+
+            for (int x = 0; x < numeros.Length; x++)
+            {
+                Console.WriteLine($"Número {x + 1}");
+                while(!int.TryParse(Console.ReadLine(), out numeros[x]))
+                {
+                    Console.WriteLine("Entrada invalida, ingrese numeros enteros");
+                }
+            }
+
+            Array.Sort(numeros);
+            Console.WriteLine("\nNúmeros ordenados de menor a mayor:");
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
---
+++ Arreglo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Arreglo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Arreglo")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a12ad5f6-2b83-432a-9d28-a02a5f11a8e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
